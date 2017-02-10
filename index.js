5206d5c31a53f8f655e147c3ad7a30769967eeb7
Rai Phairow <rai.phairow@github.com>
2017-02-10 12:54:13 -0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cpModel = require('./cp-model-mixin');
+var cpModel = require('./lib/cp-model-mixin');
 
 module.exports = function mixin (app) {
   app.loopback.modelBuilder.mixins.define('CpModelMixin', cpModel);

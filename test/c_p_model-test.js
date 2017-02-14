3d0b93971c304c555b18079016c80961ab2a1504
Rai Phairow <rai.phairow@github.com>
2017-02-14 11:42:36 -0800

    update tests

---
+++ test/c_p_model-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var mixin = require('../src/c_p_model');
+
+describe('Carrick Park model mixin', function () {
+
+  it('Should contain host name on create', function () {
+    var ctx = { req: { accessToken: "testtoken"} };
+    var Model = {};
+
+    ctx.req.get = sinon.stub().returns('testhost');
+
+    mixin(Model);
+
+    var options = Model.createOptionsFromRemotingContext(ctx);
+
+    expect(options.host).to.equal('testhost');
+    expect(optoins.token).to.equal('testtoken');
+  });
+
+});

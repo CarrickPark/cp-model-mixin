f0e46de1c8ae27ce0735ee08d7b0a29dfe9bed88
Rai Phairow <rai.phairow@github.com>
2017-02-14 12:22:08 -0800

    build break

--- src/c_p_model.js
+++ src/c_p_model.js
@@ -1,7 +1,7 @@
 /*
  * Add the host to remoting options
  */
-module.exports = function(Model, options) {
+module.exports = function(Model, options) 
   console.log("loaded CPModel")
     Model.createOptionsFromRemotingContext = function(ctx) {
       console.log(" ctx " + ctx.req.get('host'));

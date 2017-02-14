ebfe5a4820ca7adbdd94fc51e6b23a67040daa1e
Rai Phairow <rai.phairow@github.com>
2017-02-14 10:46:48 -0800

    add test

--- src/c_p_model.js
+++ src/c_p_model.js
@@ -2,8 +2,9 @@
  * Add the host to remoting options
  */
 module.exports = function(Model, options) {
-  console.log("multimodel")
+  console.log("loaded CPModel")
     Model.createOptionsFromRemotingContext = function(ctx) {
+      console.log(" ctx " + ctx.req.get('host'));
       return {
         accessToken: ctx.req.accessToken,
         host: ctx.req.get('host')
---
+++ test/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var mixin = require('../src/c_p_model');
+
+describe('Carrick Park model mixin', function () {
+
+  it('Should contain host name on create', function () {
+    
+
+    expect('aaa').to.be.ok;
+  });
+
+});

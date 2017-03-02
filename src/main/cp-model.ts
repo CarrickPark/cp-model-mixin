9e6dd6b55e0ed978ded54c06654162080977fac6
Rai Phairow <rai.phairow@github.com>
2017-03-01 22:27:31 -0800

    tsdoc

--- src/main/cp-model.ts
+++ src/main/cp-model.ts
@@ -1,13 +1,18 @@
-/*
+/**
  * Add the host to remoting options
  */
-
 import * as loopback from 'loopback';
 
 export default CPModel;
 
+/**
+ * Loopback Model mixin adds host to model context.
+ *
+ * @param Model - The model to add context.
+ * @param options The model options.
+ */
 export function CPModel(Model: loopback.Model, options: any) {
-  console.log('loaded CPModel');
+    console.log('loaded CPModel');
     Model.createOptionsFromRemotingContext = function(ctx) {
       console.log(' ctx ' + ctx.req.get('host'));
       return {

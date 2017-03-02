5df6a57583797cbefe1dbc7aa76bd9087d6b9bbd
Rai Phairow <rai.phairow@github.com>
2017-03-02 02:05:53 -0800

    updated

--- dist/main/cp-model.js
+++ dist/main/cp-model.js
@@ -1,21 +1,26 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CPModel;
-/**
- * Loopback Model mixin adds host to model context.
- *
- * @param Model - The model to add context.
- * @param options The model options.
- */
-function CPModel(Model, options) {
-    console.log('loaded CPModel');
-    Model.createOptionsFromRemotingContext = function (ctx) {
-        console.log(' ctx ' + ctx.req.get('host'));
-        return {
-            accessToken: ctx.req.accessToken,
-            host: ctx.req.get('host')
-        };
-    };
-}
-exports.CPModel = CPModel;
-;
+var cp;
+(function (cp) {
+    var mixins;
+    (function (mixins) {
+        /**
+         * Loopback Model mixin adds host to model context.
+         *
+         * @param Model - The model to add context.
+         * @param options The model options.
+         */
+        function CPModel(Model, options) {
+            console.log('loaded CPModel');
+            Model.createOptionsFromRemotingContext = function (ctx) {
+                console.log(' ctx ' + ctx.req.get('host'));
+                return {
+                    accessToken: ctx.req.accessToken,
+                    host: ctx.req.get('host')
+                };
+            };
+        }
+        mixins.CPModel = CPModel;
+        ;
+    })(mixins = cp.mixins || (cp.mixins = {}));
+})(cp || (cp = {}));
+module.exports = cp;
--- src/main/cp-model.ts
+++ src/main/cp-model.ts
@@ -3,21 +3,25 @@
  */
 import * as loopback from 'loopback';
 
-export default CPModel;
+export = cp;
 
-/**
- * Loopback Model mixin adds host to model context.
- *
- * @param Model - The model to add context.
- * @param options The model options.
- */
-export function CPModel(Model: loopback.Model, options: any) {
-    console.log('loaded CPModel');
-    Model.createOptionsFromRemotingContext = function(ctx) {
-      console.log(' ctx ' + ctx.req.get('host'));
-      return {
-        accessToken: ctx.req.accessToken,
-        host: ctx.req.get('host')
-      };
-    };
-};
+namespace cp {
+    export namespace mixins {
+        /**
+         * Loopback Model mixin adds host to model context.
+         *
+         * @param Model - The model to add context.
+         * @param options The model options.
+         */
+        export function CPModel(Model: loopback.Model, options: any) {
+            console.log('loaded CPModel');
+            Model.createOptionsFromRemotingContext = function(ctx) {
+              console.log(' ctx ' + ctx.req.get('host'));
+              return {
+                accessToken: ctx.req.accessToken,
+                host: ctx.req.get('host')
+              };
+            };
+        };
+    }
+}

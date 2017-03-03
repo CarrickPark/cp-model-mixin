14cb902bca92adfaf371940ceea65184cfb262a3
Rai Phairow <rai.phairow@github.com>
2017-03-02 16:46:04 -0800

    clean up

--- src/test/cp-model-test.ts
+++ src/test/cp-model-test.ts
@@ -5,21 +5,24 @@ import * as loopback from 'loopback';
 import CPModel = require('../main/cp-model');
 
 const expect = chai.expect;
+
 chai.use(sinonChai);
 
 describe('Carrick Park model mixin', function () {
 
-  it('Should contain host name on create', function () {
-    const ctx = { req: { accessToken: 'testtoken', get: {} } };
-    const Model: loopback.Model = { createOptionsFromRemotingContext: function (ctxin: any) { return null; }};
+    it('Should contain host name on create', function () {
 
-    ctx.req.get = sinon.stub().returns('testhost');
+        const ctx = { req: { accessToken: 'testtoken', get: {} } };
+        const Model: loopback.Model = { createOptionsFromRemotingContext: function (ctxin: any) { return null; }};
 
-    CPModel(Model, {});
-    const options = Model.createOptionsFromRemotingContext(ctx);
+        ctx.req.get = sinon.stub().returns('testhost');
 
-    expect(options.host).to.equal('testhost');
-    expect(options.accessToken).to.equal('testtoken');
-  });
+        CPModel(Model, {});
+        const options = Model.createOptionsFromRemotingContext(ctx);
+
+        expect(options.host).to.equal('testhost');
+        expect(options.accessToken).to.equal('testtoken');
+
+    });
 
 });
